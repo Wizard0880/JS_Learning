fd14823f6d6beea9de8052cc2ede591e5085e0ed
Suryansh <suryansh.dxt.18@gmail.com>
2024-10-24 19:24:28 +0530

    Changes to be committed: 	new file:   01_Basics/dataTypes_summary.js

---
+++ 01_Basics/dataTypes_summary.js
@@ -0,0 +1,26 @@
+// Data kis Tarah se memory me store hota hai uske basis pr there are two types of data types in JavaScript
+// 1. Primitive Data Types
+// 2. Non Primitive Data Types
+
+// Js is dynamically typed language .This means that variable types are determined at runtime, and you do not need to explicitly declare the type of a variable before using it. You can assign different types of values to a variable during its lifetime.
+
+/* 
+Primitive Data Types - Call by Value
+7 Types of Data Types in JavaScript
+
+1. Number
+2. String
+3. Boolean
+4. Undefined
+5. Null
+6. Symbol
+7. BigInt - last me bs n add krna hota hai
+
+*/
+
+/*
+Non Primitive Data Types - Call by Reference
+1. Object
+2. Array
+3. Function
+*/
