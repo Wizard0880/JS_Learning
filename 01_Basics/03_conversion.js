24ed651c8e8dd1b68dc835ecf13e0e5f2b7dd087
Suryansh <suryansh.dxt.18@gmail.com>
2024-10-24 18:38:44 +0530

    Changes to be committed: 	modified:   01_Basics/03_conversion.js

--- 01_Basics/03_conversion.js
+++ 01_Basics/03_conversion.js
@@ -25,3 +25,18 @@ true => 1 , false => 0
 undefined => NaN
 null => 0
 */
+
+// ************ Operations ************ //
+
+let a = 10
+let b = -a
+console.log(b) 
+
+console.log("10" + 20) // This happens because the result is followed by the data type of the first operand
+console.log(10 + "20") // Here it will be 10 + "20" = 1020 but again the same rule applies and result is followed by the data type of the first operand
+console.log("10" + "20")
+console.log(10 + 20 + "30") // This happens from left to right and 10+20 = 30 and then 30 + "30" = 3030 
+
+let c = 10
+console.log(c++)
+console.log(++c)
