0ac512401129f17a007562b3145716001a349a53
Suryansh <suryansh.dxt.18@gmail.com>
2024-10-24 19:32:21 +0530

    few things  about memory

---
+++ 01_Basics/Memory_Lessons.js
@@ -0,0 +1,8 @@
+// Stack - Primitive data types store in stack memory
+// -> we get a copy of stored thing in stack memory
+// -> changes made in original thing will not reflect in copied thing
+
+
+// Heap - Non Primitive data types store in heap memory
+// -> we get a reference of stored thing in heap memory
+// -> changes made in original thing will reflect in copied thing
